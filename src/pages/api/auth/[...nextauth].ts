7a8386a3bad18c6046e5a1bf13db22c1328d657b
Julia Rocha Belizario <jbelizario9@hotmail.com>
2021-10-30 17:54:29 -0300

    inserindo a api

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,14 @@
+import NextAuth from "next-auth"
+import Provider from "next-auth/providers"
+
+export default NextAuth({
+  
+  providers: [
+    Provider.GitHub({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    
+  ],
+  database: process.env.DATABASE_URL,
+})
